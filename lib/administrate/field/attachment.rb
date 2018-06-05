348ed36e4227cc19489ce25984f082a344eed0bc
Manuel Meurer <manuel@krautcomputing.com>
2018-06-05 23:53:39 +0200

    add url parts to attachment field

--- lib/administrate/field/attachment.rb
+++ lib/administrate/field/attachment.rb
@@ -3,6 +3,13 @@ require_relative "base"
 module Administrate
   module Field
     class Attachment < Base
+      def url_parts(namespace)
+        if options.key?(:url_parts)
+          options[:url_parts].call resource
+        else
+          [attribute, namespace, resource]
+        end
+      end
     end
   end
 end
